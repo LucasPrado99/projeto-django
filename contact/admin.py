630cdd478cad4bbb7be39070689a5bc9017f4e93
lucas.prado <lucas.prado@itmss.com.br>
2023-09-01 14:14:18 -0300

    atualizações

--- contact/admin.py
+++ contact/admin.py
@@ -3,8 +3,9 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name', 'last_name', 'phone',
+    list_display = 'id','first_name', 'last_name', 'phone', 'show',
     search_fields = 'id', 'first_name', 'last_name',
+    list_editable = 'show',
     list_per_page = 10
     list_max_show_all = 100
 
